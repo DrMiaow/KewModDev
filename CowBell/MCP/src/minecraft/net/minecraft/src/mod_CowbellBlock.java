b85349342ae36b09d9292b9114d76b06da15fb85
James Mc Parlane <drmiaow@gmail.com>
2012-01-29 13:51:34 +1100

    Added the CowBell Mod - first version...

---
+++ CowBell/MCP/src/minecraft/net/minecraft/src/mod_CowbellBlock.java
@@ -0,0 +1,118 @@
+//
+// This is Kew's second mod.
+//
+// Name : Cowbell.
+//
+// Requirements : Click - Plays sound. Spawns a cow. (1/10 chance of MooShroom). 
+// 
+//
+
+package net.minecraft.src;
+
+import java.util.Random;
+
+
+public class mod_CowbellBlock extends Block
+{
+	
+	 protected mod_CowbellBlock()
+	 {
+	        super(125, 0, Material.rock);
+	        setHardness(1.1F);
+	        setResistance(10F);
+	        setStepSound(Block.soundMetalFootstep);
+	        setBlockName("cowbell");
+	        //setLightValue(1);
+	 }
+	 
+	 
+	  public int getBlockTextureFromSide(int i)
+	    {
+	        if(i == 1)
+	        {
+	        	// Top
+	        	return Block.blockGold.getBlockTextureFromSide(0);
+	        }
+	        if(i == 0)
+	        {
+	        	// Bottom
+	            return Block.portal.blockIndexInTexture;
+	        }
+	        else
+	        {
+	        	// 2-5 sides...
+	            return blockIndexInTexture;
+	        }
+	    }
+
+	 //
+	 // What happens when someone right clicks on the block
+	 //
+	 public boolean blockActivated(World world, int i, int j, int k, EntityPlayer entityplayer)
+	 {
+
+		 // Play a sound (like a cowbell)
+		 world.playSoundAtEntity(entityplayer, "random.levelup", 1.0F, 1.0F);
+		 
+		 // The cow we are trying to create
+		 EntityLiving cow = null;
+	         
+		 // Roll the dice 
+	     int roll = world.rand.nextInt(10);
+	         
+ 	     // Kew likes 2 for a MooShroom, convention be damned..
+         if (roll == 2) 
+         {
+        	 entityplayer.addChatMessage("OMG!!! MooShroom!!!!");
+        	 
+        	 //	Set entity to a MooShroom
+        	 cow = (EntityLiving)EntityList.createEntityInWorld("MushroomCow", entityplayer.worldObj);
+         }
+         else
+         {	        	 
+        	 entityplayer.addChatMessage("We give a cow... man. :)");
+        	 
+        	 // Set entity to a Cow
+        	 cow = (EntityLiving)EntityList.createEntityInWorld("Cow", entityplayer.worldObj);	        	 
+         }
+         
+         
+         //System.out.println(String.format("Block location %s %s %s.",i,j,k));
+         //System.out.println(String.format("Player location %s %s %s.",entityplayer.posX,entityplayer.posY,entityplayer.posZ));
+         
+         // calculate 3d point half way between the block and the player. 
+         double x = ((double)i + entityplayer.posX)/2.0d;	         
+         double y = ((double)j + entityplayer.posY)/2.0d;	         
+         double z = ((double)k + entityplayer.posZ)/2.0d;
+         
+         // go up 120 blocks
+         y += 2;
+         
+         // Place pig in world at location
+         cow.setLocationAndAngles(x, y, z, 0F, 0F);
+         
+         // Create random 3d vector with values from -0.5 to 0.5
+         double dx = world.rand.nextDouble() - 0.5;
+         double dy = world.rand.nextDouble() - 0.5;
+         double dz = world.rand.nextDouble() - 0.5;
+         
+         // How fast we want to go..
+         double velocity = 1.0;
+         
+         // Multiply (scale) that vector by velocity.
+         dx *= velocity;
+         dy *= velocity;
+         dz *= velocity;
+
+         // Set the speed
+         cow.setVelocity(dx, dy, dz);
+         
+         // Add the entity to the world
+         entityplayer.worldObj.joinEntityInSurroundings(cow);
+     		
+		 return true;
+	 }
+	 
+        	 
+}
+
---
+++ CowBell/MCP/src/minecraft/net/minecraft/src/mod_CowbellMod.java
@@ -0,0 +1,139 @@
+//
+// This is Kew's first mod.
+//
+// We wanted to do CowBell, but we are following the tutorial at
+// http://www.youtube.com/watch?v=Yy2QAWOPiBI
+//
+
+package net.minecraft.src;
+
+import java.util.List;
+import java.util.Random;
+import java.util.Map;
+import net.minecraft.client.Minecraft;
+
+
+// This is the actual  block mod
+public class mod_CowbellMod extends BaseMod
+{
+	
+	// Properties
+
+	// The actual block
+	public static final Block cowbellBlock;
+	
+	// The Item...
+	public static Item cowbellItem;
+	
+	// Constructors	
+	static 
+	{
+		cowbellBlock = new mod_CowbellBlock();	
+		
+		cowbellItem = (new Item(150)).setFull3D().setItemName("cowbell");
+		
+	}
+	
+	
+    public mod_CowbellMod()
+    {
+    	//super();
+    	
+    	System.out.println("Kew's mod running constructor.");
+    	
+    	// Hook 
+		ModLoader.SetInGUIHook(this, true, true);
+		ModLoader.SetInGameHook(this, true, true);    	
+    }
+
+    // Methods
+    
+    //@Override
+    public String getVersion()
+    {
+    	return "1.0.0";
+    }
+
+	//@Override
+	public void load() 
+	{
+			System.out.println("Loading \"Kew's Cowbell Mod\" - By Kew McParlane - 2012");
+			
+	    	ModLoader.RegisterBlock(cowbellBlock);
+	    	ModLoader.AddName(cowbellBlock, "Cowbell");    	                                                                       
+	    	cowbellBlock.blockIndexInTexture = ModLoader.addOverride("/terrain.png","/cowbell_texture.png");
+	
+	    	ModLoader.AddName(cowbellItem, "Cowbell Block");	    	
+	    	cowbellItem.iconIndex = ModLoader.addOverride("/gui/items.png", "/cowbell_block.png");
+	
+	    	//
+	    	// Here we define our recipe
+	    	//
+	    	
+	    	// This is the pattern to put into the crafting table..
+	    	Object[] recipe =  new Object[] {
+	    			" S ",
+	    			"III",
+	    			"INI",
+	    			
+	    			Character.valueOf('S'),Item.silk,
+	    			Character.valueOf('I'),Item.ingotGold,
+	    			Character.valueOf('N'),Item.goldNugget
+	    	};
+
+	    	// ... and this is what we build.
+	    	//ItemStack stack = new ItemStack(cowbellBlock,1);
+	    	
+	    	ItemStack stack = new ItemStack(cowbellBlock,1);
+	    	
+	    	// Add the recipe to Minecraft.
+	    	
+	    	ModLoader.AddRecipe(stack,recipe);
+	}	
+	
+	
+	//
+	// Add too GuiContainerCreative
+	//
+	
+	// Flag that represents that your have added your item to the container
+	public boolean haveAddedToCreativeContainer = false;
+	
+	// On every tick of the GUI
+	public boolean OnTickInGUI(float f, Minecraft minecraft, GuiScreen guiscreen)
+	{		
+		// If we are on a server
+		if ((minecraft.theWorld == null) || minecraft.theWorld.multiplayerWorld) 
+		{
+			//  We don't want to do this...			
+			return true;
+		}
+		else
+		{
+			// If this is the Creative container
+			if(guiscreen instanceof GuiContainerCreative) 
+			{		
+	
+				// And we have not added it yet..
+				if (!haveAddedToCreativeContainer)
+				{
+					// Then add it...					
+					Object obj = (ContainerCreative)((GuiContainerCreative) guiscreen).inventorySlots;
+					((ContainerCreative)obj).itemList.add(new ItemStack(cowbellBlock));
+					
+					// Mark it as added..
+					haveAddedToCreativeContainer = true;
+				}
+			}
+			else
+			{
+				// Next time we are not in the GuiContainerCreative, we forget we added it
+				haveAddedToCreativeContainer = false;		
+			}
+		}
+		
+		return true;
+	}
+	
+	
+}
