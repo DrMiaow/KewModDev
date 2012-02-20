6d648bbd11feaaa7bc0262db699f20e17401fa46
James Mc Parlane <drmiaow@gmail.com>
2012-02-20 20:32:39 +1100

    Code for the 1.1.1 release.

--- CowBell/MCP/src/minecraft/net/minecraft/src/mod_CowbellBlock.java
+++ CowBell/MCP/src/minecraft/net/minecraft/src/mod_CowbellBlock.java
@@ -44,6 +44,71 @@ public class mod_CowbellBlock extends Block
 	            return blockIndexInTexture;
 	        }
 	    }
+	  
+	  
+    public void onNeighborBlockChange(World world, int i, int j, int k, int l)
+    {
+        if (l > 0 && Block.blocksList[l].canProvidePower() && world.isBlockIndirectlyGettingPowered(i, j, k))
+        {
+         // Play a sound (like a cowbell)
+   		 //world.playSoundAtEntity(entityplayer, "random.levelup", 1.0F, 1.0F);
+   		    		 
+   		  world.playSoundEffect(i,j,k, "random.levelup", 1.0F, 1.0F);
+   		    		 
+   		 // The cow we are trying to create
+   		 EntityLiving cow = null;
+   	         
+   		 // Roll the dice 
+   	     int roll = world.rand.nextInt(10);
+   	         
+    	    // Kew likes 2 for a MooShroom, convention be damned..
+            if (roll == 2) 
+            {
+            	//	Set entity to a MooShroom
+           	 	cow = (EntityLiving)EntityList.createEntityInWorld("MushroomCow", world);
+            }
+            else
+            {	        	 
+           	 	// Set entity to a Cow
+           	 	cow = (EntityLiving)EntityList.createEntityInWorld("Cow", world);	        	 
+            }
+            
+            
+            //System.out.println(String.format("Block location %s %s %s.",i,j,k));
+            //System.out.println(String.format("Player location %s %s %s.",entityplayer.posX,entityplayer.posY,entityplayer.posZ));
+            
+            // calculate 3d point half way between the block and the player. 
+            double x = ((double)i)/1.0d;	         
+            double y = ((double)j)/1.0d;	         
+            double z = ((double)k)/1.0d;
+            
+            // go up 120 blocks
+            y += 2;
+            
+            // Place pig in world at location
+            cow.setLocationAndAngles(x, y, z, 0F, 0F);
+            
+            // Create random 3d vector with values from -0.5 to 0.5
+            double dx = world.rand.nextDouble() - 0.5;
+            double dy = world.rand.nextDouble() - 0.5;
+            double dz = world.rand.nextDouble() - 0.5;
+            
+            // How fast we want to go..
+            double velocity = 1.0;
+            
+            // Multiply (scale) that vector by velocity.
+            dx *= velocity;
+            dy *= velocity;
+            dz *= velocity;
+
+            // Set the speed
+            cow.setVelocity(dx, dy, dz);
+            
+            // Add the entity to the world
+            world.joinEntityInSurroundings(cow);	
+        }
+    }
+	  
 
 	 //
 	 // What happens when someone right clicks on the block
@@ -66,14 +131,14 @@ public class mod_CowbellBlock extends Block
         	 entityplayer.addChatMessage("OMG!!! MooShroom!!!!");
         	 
         	 //	Set entity to a MooShroom
-        	 cow = (EntityLiving)EntityList.createEntityInWorld("MushroomCow", entityplayer.worldObj);
+        	 cow = (EntityLiving)EntityList.createEntityInWorld("MushroomCow", world);
          }
          else
          {	        	 
         	 entityplayer.addChatMessage("We give a cow... man. :)");
         	 
         	 // Set entity to a Cow
-        	 cow = (EntityLiving)EntityList.createEntityInWorld("Cow", entityplayer.worldObj);	        	 
+        	 cow = (EntityLiving)EntityList.createEntityInWorld("Cow", world);	        	 
          }
          
          
@@ -108,7 +173,7 @@ public class mod_CowbellBlock extends Block
          cow.setVelocity(dx, dy, dz);
          
          // Add the entity to the world
-         entityplayer.worldObj.joinEntityInSurroundings(cow);
+         world.joinEntityInSurroundings(cow);
      		
 		 return true;
 	 }
